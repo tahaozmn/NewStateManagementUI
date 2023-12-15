6207f653ad53626f74dfffb6f62d19a43781463c
Taha ÖZMEN <139132956+tahaozmn@users.noreply.github.com>
2023-12-15 10:32:06 +0300

    Add files via upload

---
+++ NewStateManagementUI/NewStateManagementUIApp.swift
@@ -0,0 +1,22 @@
+//
+//  NewStateManagementUIApp.swift
+//  NewStateManagementUI
+//
+//  Created by Taha Özmen on 15.12.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NewStateManagementUIApp: App {
+  
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(DarkTheme())
+        }
+      
+    }
+}
